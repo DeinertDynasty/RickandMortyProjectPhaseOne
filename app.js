a694bcf6cc84f848e316cea02bff45018cff0c2c
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-07-03 14:49:35 -0400

    update js to allow for more character info to show

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
-// Get the input field and results div
-const searchBox = document.getElementById('searchBox');
-const results = document.getElementById('results');
+// Fetch the characters from the API
+fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`)
+    .then(response => response.json())
+    .then(data => {
+        // Loop through the results and add them to the page
+        data.results.forEach(character => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <h2>${character.name}</h2>
+                <img src="${character.image}" alt="${character.name}">
+                <p id="info${character.id}" style="display: none;">
+                    Status: ${character.status}<br>
+                    Species: ${character.species}<br>
+                    Gender: ${character.gender}<br>
+                    Location: ${character.location.name}
+                </p>
+            `;
+            results.appendChild(div);
 
-// Add an event listener to the input field
-searchBox.addEventListener('keyup', (e) => {
-    // Clear the results
-    results.innerHTML = '';
-
-    // Get the search term
-    const searchTerm = e.target.value;
-
-    // Fetch the characters from the API
-    fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`)
-        .then(response => response.json())
-        .then(data => {
-            // Loop through the results and add them to the page
-            data.results.forEach(character => {
-                const div = document.createElement('div');
-                div.innerHTML = `
-                    <h2>${character.name}</h2>
-                    <img src="${character.image}" alt="${character.name}">
-                    <p id="info${character.id}" style="display: none;">${character.status} - ${character.species}</p>
-                `;
-                results.appendChild(div);
-
-                // Add click event listener to the div
-                div.addEventListener('click', () => {
-                    const info = document.getElementById(`info${character.id}`);
-                    if (info.style.display === 'none') {
-                        info.style.display = 'block';
-                    } else {
-                        info.style.display = 'none';
-                    }
-                });
-
-                // Add mouseover event listener to the div
-                div.addEventListener('mouseover', () => {
-                    div.style.backgroundColor = '#5a0fb4';
-                });
-
-                // Add mouseout event listener to the div
-                div.addEventListener('mouseout', () => {
-                    div.style.backgroundColor = '';
-                });
+            // Add click event listener to the div
+            div.addEventListener('click', () => {
+                const info = document.getElementById(`info${character.id}`);
+                if (info.style.display === 'none') {
+                    info.style.display = 'block';
+                } else {
+                    info.style.display = 'none';
+                }
             });
-        })
-        .catch(error => {
-            console.error('Error:', error);
+
+            // Add mouseover event listener to the div
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = '#5a0fb4';
+            });
+
+            // Add mouseout event listener to the div
+            div.addEventListener('mouseout', () => {
+                div.style.backgroundColor = '';
+            });
         });
-});
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
